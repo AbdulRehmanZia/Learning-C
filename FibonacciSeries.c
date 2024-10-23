9f7e3c88938559afb04b4aa304f5d317ba12a24b
AbdulRehmanZia <153487879+AbdulRehmanZia@users.noreply.github.com>
2024-10-23 18:34:04 +0500

    Update FibonacciSeries.c

--- FibonacciSeries.c
+++ FibonacciSeries.c
@@ -16,3 +16,22 @@ int main()
     
     return 0;
 }
+
+/* For 10 digits limit 
+
+#include<stdio.h>
+
+int main()
+{
+    int t1 = 0, t2 = 1, t3= 0,n=3;
+    printf("Fibonacci Series: %d %d",t1, t2);
+    t3 = t1 + t2;
+    while( n<=10) {
+    printf(" %d",t3);
+    t1=t2;
+    t2=t3;
+    t3 = t1+t2;
+    n++;
+    }
+    return 0;
+} */
