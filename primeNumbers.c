60fc2f36cf3b50d76d20cbe87cde62c86e45153f
AbdulRehmanZia <153487879+AbdulRehmanZia@users.noreply.github.com>
2024-10-29 00:36:17 +0500

    Update

---
+++ primeNumbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+int num;
+bool isPrime;
+    printf("Enter Your Number: ");
+    scanf("%d",&num);
+    
+    for (int j=1; j<num; j++) {
+    if (num%j == 0) {
+    isPrime = true;
+    printf("The number is prime");
+    }
+    else {
+    printf("The number isn't prime");
+    }
+    }
+    return 0;
+}
