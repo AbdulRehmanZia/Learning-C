8cdb702cc5757b36717f877a2df4d38ce3546bbc
AbdulRehmanZia <153487879+AbdulRehmanZia@users.noreply.github.com>
2024-11-21 18:36:21 +0500

    updated C

---
+++ array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[10] = {3,8,4,9,5,8,0,1,4,6};
+    printf("Array=>");
+    for (int j=0; j<=10; j++) {
+    printf(" %d",arr[j]);
+    }
+    return 0;
+}
