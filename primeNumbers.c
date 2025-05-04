7d10cb3650e3afda7e04b8ab69d13245a2184f75
AbdulRehmanZia <153487879+AbdulRehmanZia@users.noreply.github.com>
2025-05-04 19:07:34 +0500

    Update primeNumbers.c

--- primeNumbers.c
+++ primeNumbers.c
@@ -7,15 +7,18 @@ int num;
 bool isPrime;
     printf("Enter Your Number: ");
     scanf("%d",&num);
-    
+
     for (int j=1; j<num; j++) {
     if (num%j == 0) {
     isPrime = true;
+    }
+    }
+    if(isPrime){
     printf("The number is prime");
     }
     else {
     printf("The number isn't prime");
     }
-    }
+    
     return 0;
 }
